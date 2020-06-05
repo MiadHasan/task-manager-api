dd40616d5eaf6fdbdbc4b3e9bf2fa5acfb82bfd8
Miad Hasan <mdhasan9.sy@gmail.com>
2020-06-05 09:12:22 +0600

    Init commit

---
+++ src/emails/account.js
@@ -0,0 +1,26 @@
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'u1704118@student.cuet.ac.bd',
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    })
+}
+
+const sendGoodbyeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: 'u1704118@student.cuet.ac.bd',
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. Let us know what can we do for you.`
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendGoodbyeEmail
+}
